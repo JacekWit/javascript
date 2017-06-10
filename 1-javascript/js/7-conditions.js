f0693b1c0f6aecf48295504f308873e6f903e10f
JacekWit <jacek_witkowski@interia.pl>
2017-06-10 12:35:13 +0200

    switch

--- 1-javascript/js/7-conditions.js
+++ 1-javascript/js/7-conditions.js
@@ -16,3 +16,53 @@ if (wzrostOlgi>wzrostMateusz){
 }else{
     console.log("Ola jest niższa");
 }
+
+/*Warunek if else if*/
+if(wzrostOlgi>wzrostMateusz){
+   console.log("Ola jest wyższa:)"); 
+}else if (wzrostOlgi ==wzrostMateusz){
+    console.log("Olga jest wysoka jak Mateusz");
+}
+        else{
+            console.log("Olga jest niższa");
+        }
+    /*Warunek switch*/
+        var kolor = 'zielony';
+        switch (kolor){
+            case 'czerwony':
+                console.log('kolor czerwony')
+                
+            case 'zielony':
+                console.log('kolor zielony')
+                
+            case 'niebieski':
+                console.log ('kolor niebieski');
+                break;
+            default:
+                console.log('inny kolor');
+                     }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
