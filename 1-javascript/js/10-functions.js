421ce747ded58498268a06d19dfacb92efcf6077
JacekWit <jacek_witkowski@interia.pl>
2017-06-10 14:57:35 +0200

    init

--- 1-javascript/js/10-functions.js
+++ 1-javascript/js/10-functions.js
@@ -1,1 +1,20 @@
-'use strict'
+'use strict';
+
+
+/*
+function multiply(parametr1, parametr2, parametr3){
+    var result;
+    result =  parametr1 + parametr2 + parametr3;
+    return result;
+}
+
+var wynik = multiply(3, 9, 10);
+console.log("Wynik: " + wynik);
+
+//var funkcja = function (){
+// return "wynik"
+*/
+var funkcja = function (){
+    return "wynik:)";
+    
+};
