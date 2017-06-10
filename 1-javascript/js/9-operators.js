f06de2f9709d12f3553639afd143e68637fac348
JacekWit <jacek_witkowski@interia.pl>
2017-06-10 14:17:50 +0200

    operations

---
+++ 1-javascript/js/9-operators.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var liczba1 = 13, liczba2 = 5;
+
+var wynik;
+
+wynik = liczba1 % liczba2;
+console.log(wynik);
+
+var licz = 2;
+licz +=3;
+//licz = licz + 3;
+
+console.log(licz);
