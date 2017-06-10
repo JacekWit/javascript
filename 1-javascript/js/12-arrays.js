0df03608b1d97a12bed70a641c19ab432a4d7461
JacekWit <jacek_witkowski@interia.pl>
2017-06-10 15:55:08 +0200

    init

--- 1-javascript/js/12-arrays.js
+++ 1-javascript/js/12-arrays.js
@@ -5,6 +5,7 @@ var imiona = ['Monika', 'Krystian', 'Łukasz'];
 imiona[3] = 'Tania';
 imiona[2] = 'Ela';
 //
+/*
 imiona.push('Geralt');
 
 console.log(imiona);
@@ -15,3 +16,12 @@ console.log(imiona);
 
 console.log(imiona.unshift('Robert'));
 console.log(imiona);
+*/
+
+console.log(imiona);
+console.log(imiona.length);
+
+for (var i = 0; i <imiona.length ; i++) {
+    console.log(imiona[i]);
+}
+
